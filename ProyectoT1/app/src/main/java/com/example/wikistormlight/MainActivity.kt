8cb8e6351337414be06f7d7cf81750cca18dfed2
A23PabloMT <a23pablomt@a26eql19.sanclemente.local>
2024-11-27 13:45:11 +0100

    Navegacion lateral y ajustes

--- ProyectoT1/app/src/main/java/com/example/wikistormlight/MainActivity.kt
+++ ProyectoT1/app/src/main/java/com/example/wikistormlight/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.wikistormlight
 
 import android.os.Bundle
+import android.widget.GridView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -16,44 +17,41 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.example.wikistormlight.ui.theme.WikiStormlightTheme
-import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Arrangement.Center
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.paddingFromBaseline
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Surface
-import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -76,7 +74,10 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
 
-    Surface(color = Color(0xFFD9D9D9), modifier = Modifier.paddingFromBaseline(0.dp, 200.dp)){
+    var drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
+    Surface(color = Color(0xFFD9D9D9)){
         // Create a boolean variable
         // to store the display menu state
         var mDisplayMenu by remember { mutableStateOf(false) }
@@ -87,7 +88,14 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         // Creating a Top bar
         TopAppBar(
             navigationIcon = {
-                IconButton(onClick = { /*TODO*/ }) {
+                IconButton(onClick = {
+                    scope.launch {
+                        drawerState.apply {
+                            if (isClosed) open() else close()
+                        }
+                    }
+
+                }) {
                     Icon(imageVector = Icons.Rounded.Menu, contentDescription = null)
                 }
             },
@@ -106,15 +114,18 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                 ) {}
             }
         )
-        Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxWidth()){
+        Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxSize()){
             Image(
                 painter = painterResource(R.drawable.choose_order),
                 contentDescription = ("Str"),
-                modifier = Modifier.padding(100.dp)
+                modifier = Modifier
+                    .padding(100.dp)
                     .width(300.dp)
                     .height(200.dp)
             )
-            SearchBar(modifier = Modifier.width(300.dp).offset(0.dp, (-120).dp),
+            SearchBar(modifier = Modifier
+                .width(300.dp)
+                .offset(0.dp, (-120).dp),
                 query = "Buscar...",
                 onQueryChange = {},
                 onSearch = {},
@@ -126,30 +137,93 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                 trailingIcon = {
                     Icon(imageVector = Icons.Default.Search, contentDescription = null)
                 }) {}
-            Row(modifier = Modifier.width(300.dp)){
-                Box(modifier = Modifier.height(230.dp)){
-                    Box(modifier = Modifier.width(100.dp)
-                        .height(100.dp)
-                        .clip(RoundedCornerShape(20.dp))
-                        .background(Color.White)
-                        .align(Alignment.TopStart)){
-                        Image(
-                            painter = painterResource(R.drawable.choose_order),
-                            contentDescription = ("Str"),
-                            modifier = Modifier.fillMaxSize()
-                        )
-                    }
-                    Box(modifier = Modifier.width(100.dp)
-                        .height(20.dp)
-                        .clip(RoundedCornerShape(20.dp))
-                        .background(Color.White)
-                        .align(Alignment.CenterEnd)){
-                        Text("Sigzil", modifier = Modifier.align(Alignment.Center), fontSize = 13.sp)
-                    }
+            Row(modifier = Modifier.width(300.dp).height(200.dp).offset(0.dp, (-50).dp)){
+                LazyVerticalGrid(
+                    columns = GridCells.Fixed(2), // 2 columns grid
+                    modifier = Modifier.padding(16.dp)
+                ) {
+                    listOf(items(1) {
+                        Box(
+                            modifier = Modifier
+                                .height(270.dp)
+                        ) {
+                            Box(
+                                modifier = Modifier
+                                    .width(120.dp)
+                                    .height(120.dp)
+                                    .clip(RoundedCornerShape(20.dp))
+                                    .background(Color.White)
+                                    .align(Alignment.TopCenter)
+                            ) {
+                                Image(
+                                    painter = painterResource(id = R.drawable.choose_order), // Use your resource
+                                    contentDescription = "Image",
+                                    modifier = Modifier.fillMaxSize()
+                                )
+                            }
+                            Box(
+                                modifier = Modifier
+                                    .width(120.dp)
+                                    .height(20.dp)
+                                    .clip(RoundedCornerShape(20.dp))
+                                    .background(Color.White)
+                                    .align(Alignment.Center)
+                            ) {
+                                Text(
+                                    "Sigzil",
+                                    modifier = Modifier.align(Alignment.Center),
+                                    fontSize = 13.sp,
+                                    fontWeight = FontWeight.Bold
+                                )
+                            }
+                        }
+                    },
+                    items(1) {
+                        Box(
+                            modifier = Modifier
+                                .height(270.dp)
+                        ) {
+                            Box(
+                                modifier = Modifier
+                                    .width(120.dp)
+                                    .height(120.dp)
+                                    .clip(RoundedCornerShape(20.dp))
+                                    .background(Color.White)
+                                    .align(Alignment.TopCenter)
+                            ) {
+                                Image(
+                                    painter = painterResource(id = R.drawable.choose_order), // Use your resource
+                                    contentDescription = "Image",
+                                    modifier = Modifier.fillMaxSize()
+                                )
+                            }
+                            Box(
+                                modifier = Modifier
+                                    .width(120.dp)
+                                    .height(20.dp)
+                                    .clip(RoundedCornerShape(20.dp))
+                                    .background(Color.White)
+                                    .align(Alignment.Center)
+                            ) {
+                                Text(
+                                    "Kaladin",
+                                    modifier = Modifier.align(Alignment.Center),
+                                    fontSize = 13.sp,
+                                    fontWeight = FontWeight.Bold
+                                )
+                            }
+                        }
+                    })
                 }
             }
         }
     }
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet { /* Drawer content */ }
+        }
+    ) {}
 }
 
 
